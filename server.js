3eaeb61ebec8e89a77c10f3c2f51f06cc782fc28
Allison Haver <allisonbhaver@gmail.com>
2020-12-19 22:56:09 -0500

    trying to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
