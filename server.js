3822c85d8327880e968946cbd27ed98439c37f1e
Allison Haver <allisonbhaver@gmail.com>
2020-12-19 23:32:14 -0500

    test

--- server.js
+++ server.js
@@ -1,17 +1,80 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3306;
+const fs = require("fs");
+const path = require("path");
+const database = require("./db/db")
 
-app.use(express.urlencoded({ extended: true }));
+var app = express();
+var PORT = process.env.PORT || 3306;
+
+
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
 
-handleRequest = (req, res) => {
-    res.end("Success! Path Hit: " + req.url);
-}
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
+
+//
+app.route("/api/notes")
+    .get(function (req, res) {
+        res.json(database);
+    })
+
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+        let highestId = 99;
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
+
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        }
+       
+        newNote.id = highestId + 1;
+     
+        database.push(newNote)
+
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        });
+   
+        res.json(newNote);
+    });
+
+app.delete("/api/notes/:id", function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+ 
+    for (let i = 0; i < database.length; i++) {
+
+        if (database[i].id == req.params.id) {
+         
+            database.splice(i, 1);
+            break;
+        }
+    }
+ 
+    fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Your note was deleted!");
+        }
+    });
+    res.json(database);
+});
 
 app.listen(PORT, function () {
-    console.log("App is listening on PORT: " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
